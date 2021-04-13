01c9b44f7af29cc349ccaa81f15ebb2609515b78
Ethereal-bang <70147063+Ethereal-bang@users.noreply.github.com>
2021-04-13 19:50:00 +0800

    Update Question answer

--- find str in arr.c
+++ find str in arr.c
@@ -4,7 +4,7 @@ int main(void)
 {
     char user[5];
     int i, find = 0;
-    char list[][6] = {"abc", "bbc", "ccc", "Hello", "John", "Tome", "\0"};  // 为什么是 6 不能是 5
+    char list[][6] = {"abc", "bbc", "ccc", "Hello", "John", "Tome", "\0"};  // 为什么是 6 不能是 5，因为 \0 
     printf("请输入一行字符：");
     gets(user);
     for (i = 0; i < 6; i++)
